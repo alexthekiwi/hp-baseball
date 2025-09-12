42ee563668ed9f88315eebdf3f9d8885cd55cee1
Alex Clark <hello@alexclark.co.nz>
2025-09-13 11:14:10 +1200

    Store WIP

--- content/collections/pages/cart.md
+++ content/collections/pages/cart.md
@@ -1,5 +1,5 @@
 ---
-id: 1133ff88-65ef-49bf-bee3-277f0ed271b6
+id: b0b73b06-4a2a-4368-8222-49521b3115f1
 blueprint: page
 title: Cart
 hide_breadcrumbs: false
@@ -9,5 +9,6 @@ seo_canonical_type: entry
 sitemap_change_frequency: weekly
 sitemap_priority: 0.5
 updated_by: f727c14d-33d9-4ccd-9de0-fdbe1661319b
-updated_at: 1757714238
+updated_at: 1757717988
+template: cart
 ---
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ Route::statamic('help', 'docs/index', [
 
 Route::redirect('/login', '/admin', 302)->name('login');
 
-Route::statamic('cart', 'cart')->name('cart');
 Route::statamic('checkout', 'checkout.index', ['title' => 'Checkout', 'layout' => 'checkout.layout'])->name('checkout');
 Route::statamic('checkout/confirmation', 'checkout.confirmation', ['title' => 'Order Confirmation', 'layout' => 'checkout.layout'])
     ->name('checkout.confirmation')
